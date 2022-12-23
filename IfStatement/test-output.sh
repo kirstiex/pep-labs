3872b49e4f03360320a92bb9c4337b628ff1d86b
Kirstie Goggans <ikirstiew@gmail.com>
2022-12-23 23:33:35 +0000

    Saved progress on BooleanOperators 2022-12-23 23:33:35 +0000

--- IfStatement/test-output.sh
+++ IfStatement/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running IfStatementTest
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.04 sec
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.039 sec
 
 Results :
 
