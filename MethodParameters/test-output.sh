cca8b228ecf9a3c2249b9c5b92e6cc05958860e0
Kirstie Goggans <ikirstiew@gmail.com>
2022-12-23 17:53:18 +0000

    Saved progress on MethodParameters 2022-12-23 17:53:18 +0000

--- MethodParameters/test-output.sh
+++ MethodParameters/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running ParrotTest
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.055 sec
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.096 sec
 
 Results :
 
