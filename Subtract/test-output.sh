befab0dbb298d99040dd5e143bad908fc5ec1305
Kirstie Goggans <ikirstiew@gmail.com>
2022-12-23 23:49:17 +0000

    Saved progress on Subtract 2022-12-23 23:49:17 +0000

--- Subtract/test-output.sh
+++ Subtract/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running SubtractTest
-Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.046 sec
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.044 sec
 
 Results :
 
