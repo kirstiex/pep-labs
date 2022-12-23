8b1c7c9adefd606229d054bf346e474ce93eb545
Kirstie Goggans <ikirstiew@gmail.com>
2022-12-23 22:46:10 +0000

    Saved progress on CombineStrings 2022-12-23 22:46:10 +0000

--- CombineStrings/test-output.sh
+++ CombineStrings/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running CombineStringsTest
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.103 sec
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.061 sec
 
 Results :
 
