a5700cd8bf9019faf4162043d40642d894494f64
Kirstie Goggans <ikirstiew@gmail.com>
2022-12-23 22:19:14 +0000

    Saved progress on Comparisons 2022-12-23 22:19:14 +0000

---
+++ Comparisons/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running ComparatorsTest
+Tests run: 16, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.184 sec
+
+Results :
+
+Tests run: 16, Failures: 0, Errors: 0, Skipped: 0
+
