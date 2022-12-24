01351bd7867ed79f404586514a4adaf6a2905677
Kirstie Goggans <ikirstiew@gmail.com>
2022-12-24 00:40:43 +0000

    Saved progress on Constructors 2022-12-24 00:40:43 +0000

--- Constructors/test-output.sh
+++ Constructors/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running PlayerTest
-Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.092 sec
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.04 sec
 
 Results :
 
