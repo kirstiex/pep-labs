5fc2953816658958a32ea9a70c287a7231c9e22c
Kirstie Goggans <ikirstiew@gmail.com>
2022-12-27 01:41:21 +0000

    Saved progress on Arrays 2022-12-27 01:41:21 +0000

--- Arrays/test-output.sh
+++ Arrays/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running ArrayBasicsTest
-Tests run: 9, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.048 sec
+Tests run: 9, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.056 sec
 
 Results :
 
