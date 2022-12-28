3f54dba3fada2178f3621a6a3e66b6824b098b3d
Kirstie Goggans <ikirstiew@gmail.com>
2022-12-28 01:31:33 +0000

    Saved progress on SetAllArrayElementsToN 2022-12-28 01:31:33 +0000

--- SetAllArrayElementsToN/test-output.sh
+++ SetAllArrayElementsToN/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running SetAllArrayElementsTest
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.364 sec
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.166 sec
 
 Results :
 
