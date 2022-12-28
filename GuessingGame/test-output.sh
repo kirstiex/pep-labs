2ad302aec8a50cecf321d470835fa116df99536d
Kirstie Goggans <ikirstiew@gmail.com>
2022-12-28 23:14:22 +0000

    Saved progress on GuessingGame 2022-12-28 23:14:22 +0000

---
+++ GuessingGame/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running GameServiceTest
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.055 sec
+
+Results :
+
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0
+
