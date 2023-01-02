cef6406dd29bed71f7c2a1294c5166aee1fbeeb9
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-02 17:17:28 +0000

    Saved progress on Exceptions 2023-01-02 17:17:28 +0000

--- Exceptions/test-output.sh
+++ Exceptions/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running ExceptionTest
-Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.043 sec
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.042 sec
 
 Results :
 
