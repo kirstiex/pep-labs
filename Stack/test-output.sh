18d12485c8653bfa9dab2b864a95b3bc317daa80
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-15 07:29:47 +0000

    Saved progress on Stack 2023-01-15 07:29:47 +0000

--- Stack/test-output.sh
+++ Stack/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running StackImplementationExerciseTest
-Tests run: 7, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.093 sec
+Tests run: 7, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.053 sec
 
 Results :
 
