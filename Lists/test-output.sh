c59f14f1667313ba64903463a9e88749506d4dfa
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-15 06:52:36 +0000

    Saved progress on Lists 2023-01-15 06:52:36 +0000

--- Lists/test-output.sh
+++ Lists/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running ListExerciseTest
-Tests run: 11, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.041 sec
+Tests run: 11, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.04 sec
 
 Results :
 
