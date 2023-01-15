b171dfa9100c2f00e3ef55a0955ddbcf280c3cd4
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-15 07:18:05 +0000

    Saved progress on Map 2023-01-15 07:18:05 +0000

--- Map/test-output.sh
+++ Map/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running MapExerciseTest
-Tests run: 7, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.042 sec
+Tests run: 7, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.043 sec
 
 Results :
 
