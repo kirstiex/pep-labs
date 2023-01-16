7b69dcf256576c6cbff0c6fca4e21530cdd6e432
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-16 00:21:19 +0000

    Saved progress on Average 2023-01-16 00:21:19 +0000

--- Average/test-output.sh
+++ Average/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running AverageTest
-Tests run: 4, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.038 sec
+Tests run: 4, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.041 sec
 
 Results :
 
