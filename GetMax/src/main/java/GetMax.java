fb42147d41812287541459671405b29b91ed9e93
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-16 00:39:25 +0000

    Saved progress on GetMax 2023-01-16 00:39:25 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -6,7 +6,14 @@ public class GetMax {
      * @param arr an array of ints to be manipulated.
      * @return the largest value in arr.
      */
+    
     public int max(int[] arr){
-        return -1;
+        int maxNum = arr[0];
+        for (int i =0; i < arr.length; i++){
+            if (arr[i] > maxNum){
+                maxNum = arr[i];
+            }
+        }
+        return maxNum;
     }
 }
