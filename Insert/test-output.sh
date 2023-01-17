c4b739f4be99e2a1ebecec7120e235c897f8fec7
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-17 18:25:19 +0000

    Saved progress on Insert 2023-01-17 18:25:19 +0000

--- Insert/test-output.sh
+++ Insert/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running InsertARecordTest
-Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.558 sec
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.326 sec
 
 Results :
 
