d2741d57c33041043924d2d8f1dccc7354fa404f
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-17 18:53:58 +0000

    Saved progress on SelectWhere 2023-01-17 18:53:58 +0000

--- SelectWhere/test-output.sh
+++ SelectWhere/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running FilterRecordsTest
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.675 sec
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.74 sec
 
 Results :
 
