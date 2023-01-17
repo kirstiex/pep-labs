e30a0c3cf6ac307eb9fee018fb456fde34f5a0f7
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-17 21:10:01 +0000

    Saved progress on SelectWhereAndOr 2023-01-17 21:10:01 +0000

---
+++ SelectWhereAndOr/problem3.sql
@@ -0,0 +1,1 @@
+SELECT * FROM employee WHERE NOT first_name = 'Steve' and salary > 50000 ;
---
+++ SelectWhereAndOr/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running FilteringLogicTest
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.481 sec
+
+Results :
+
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0
+
