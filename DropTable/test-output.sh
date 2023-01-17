0fe6b31b79cb82a64b7a0a50a5b38874139cf8c0
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-17 18:15:45 +0000

    Saved progress on DropTable 2023-01-17 18:15:45 +0000

---
+++ DropTable/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running DropATableTest
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.357 sec
+
+Results :
+
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0
+
