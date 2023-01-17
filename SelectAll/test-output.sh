940636630bf390f145abbe7cead1e6f663e527ac
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-17 18:27:26 +0000

    Saved progress on SelectAll 2023-01-17 18:27:26 +0000

--- SelectAll/test-output.sh
+++ SelectAll/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running GetAllSongsTest
-Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.378 sec
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.363 sec
 
 Results :
 
