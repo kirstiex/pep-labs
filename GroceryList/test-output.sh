42de01196ae21398b8890d93c370b76726573561
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-17 22:33:19 +0000

    Saved progress on GroceryList 2023-01-17 22:33:19 +0000

--- GroceryList/test-output.sh
+++ GroceryList/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running GroceryRepositoryTest
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.362 sec
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.481 sec
 
 Results :
 
