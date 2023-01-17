d0bd108b58b7e0f83467c8b47545cbd7ab6cfd0e
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-17 21:17:45 +0000

    Saved progress on OrderBy 2023-01-17 21:17:45 +0000

---
+++ OrderBy/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running OrderResultSetTest
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.512 sec
+
+Results :
+
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0
+
