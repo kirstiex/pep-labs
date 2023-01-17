221438e2cfd57e9b78cfc3594620f7d87e392e58
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-17 21:41:08 +0000

    Saved progress on ArraysAreEqual 2023-01-17 21:41:08 +0000

---
+++ ArraysAreEqual/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running ArraysAreEqualTest
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.041 sec
+
+Results :
+
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0
+
