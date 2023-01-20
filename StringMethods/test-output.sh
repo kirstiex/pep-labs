9a35a64e202f6a66114f82ca9946c27c8b5c326c
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-20 02:54:30 +0000

    Saved progress on StringMethods 2023-01-20 02:54:30 +0000

--- StringMethods/test-output.sh
+++ StringMethods/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running SimpleStringMethodsTest
-Tests run: 5, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.042 sec
+Tests run: 5, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.092 sec
 
 Results :
 
