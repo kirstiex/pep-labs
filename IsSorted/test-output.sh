a463b21d7a532b2d6ac850cf9a582b084aee11e2
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-20 19:50:40 +0000

    Saved progress on IsSorted 2023-01-20 19:50:40 +0000

--- IsSorted/test-output.sh
+++ IsSorted/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running IsSortedTest
-Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.049 sec
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.039 sec
 
 Results :
 
