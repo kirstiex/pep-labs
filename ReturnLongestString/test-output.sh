a6ddab97c38ca2cd78d4e30a63fe02f966a5db40
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-20 21:09:23 +0000

    Saved progress on ReturnLongestString 2023-01-20 21:09:23 +0000

--- ReturnLongestString/test-output.sh
+++ ReturnLongestString/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running ReturnLongestStringTest
-Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.041 sec
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.049 sec
 
 Results :
 
