4be0c7594915af0cda1bf6622bb7eae8355e3af8
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-20 02:49:32 +0000

    Saved progress on GroupBy 2023-01-20 02:49:32 +0000

--- GroupBy/test-output.sh
+++ GroupBy/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running GroupByTest
-Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.52 sec
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.707 sec
 
 Results :
 
