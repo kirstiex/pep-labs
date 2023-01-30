ff5f2214d3fe8c23ea4957c4b39bb4fd51653e53
Kirstie Goggans <ikirstiew@gmail.com>
2023-01-30 01:14:54 +0000

    Saved progress on OuterJoin 2023-01-30 01:14:54 +0000

--- OuterJoin/test-output.sh
+++ OuterJoin/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running OuterJoinTest
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.417 sec
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.423 sec
 
 Results :
 
