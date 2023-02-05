10cf2d2a3143926c35dc88cf7e7ac3429b6fc0bd
Kirstie Goggans <ikirstiew@gmail.com>
2023-02-05 07:03:05 +0000

    Saved progress on Library 2023-02-05 07:03:05 +0000

--- Library/test-output.sh
+++ Library/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running LibraryAppTest
-Tests run: 13, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 1.416 sec
+Tests run: 13, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 1.986 sec
 
 Results :
 
