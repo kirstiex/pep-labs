f8d85cb7e5b789de72aded846d6d54b1e37628dc
Kirstie Goggans <ikirstiew@gmail.com>
2023-02-08 19:33:00 +0000

    Saved progress on Library 2023-02-08 19:33:00 +0000

--- FlightTracker/test-output.sh
+++ FlightTracker/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running FlightAppTest
-Tests run: 13, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 1.155 sec
+Tests run: 13, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.926 sec
 
 Results :
 
