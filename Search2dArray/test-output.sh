6f09fda85104a203ca08acfe7ede764717c350ef
Kirstie Goggans <ikirstiew@gmail.com>
2023-02-09 14:39:04 +0000

    Saved progress on Search2dArray 2023-02-09 14:39:04 +0000

---
+++ Search2dArray/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running Search2dArrayTest
+Tests run: 5, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.056 sec
+
+Results :
+
+Tests run: 5, Failures: 0, Errors: 0, Skipped: 0
+
