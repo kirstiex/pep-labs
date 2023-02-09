2774a77852467b26632f2481937cbe72268fb012
Kirstie Goggans <ikirstiew@gmail.com>
2023-02-09 15:57:03 +0000

    Saved progress on Overriding 2023-02-09 15:57:03 +0000

--- Overriding/test-output.sh
+++ Overriding/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running DogTest
-Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.042 sec
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.123 sec
 
 Results :
 
