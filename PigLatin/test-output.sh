8f24604b0c1864363c93adb6b8fd182205b6f9f9
Kirstie Goggans <ikirstiew@gmail.com>
2023-02-09 23:34:16 +0000

    Saved progress on PigLatin 2023-02-09 23:34:16 +0000

--- PigLatin/test-output.sh
+++ PigLatin/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running PigLatinTest
-Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.059 sec
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.063 sec
 
 Results :
 
