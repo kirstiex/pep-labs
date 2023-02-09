6109294ccf1ce6acf7cadfd1417f0d4157e72769
Kirstie Goggans <ikirstiew@gmail.com>
2023-02-09 15:39:55 +0000

    Saved progress on ArrayAbsoluteValue 2023-02-09 15:39:55 +0000

--- ArrayAbsoluteValue/src/main/java/ArrayAbsoluteValue.java
+++ ArrayAbsoluteValue/src/main/java/ArrayAbsoluteValue.java
@@ -9,6 +9,11 @@ public class ArrayAbsoluteValue {
      * @return the absolute value array of nums.
      */
     public int[] getArrayAbs(int[] nums){
-        return null;
+        int[] result = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            result[i] = Math.abs(nums[i]);
+        }
+
+        return result;
     }
 }
