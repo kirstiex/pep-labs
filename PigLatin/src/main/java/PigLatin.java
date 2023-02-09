0dd1f190ccd65cb9ce3a6595ed812f0fb4982bd6
Kirstie Goggans <ikirstiew@gmail.com>
2023-02-09 23:34:07 +0000

    Saved progress on PigLatin 2023-02-09 23:34:07 +0000

--- PigLatin/src/main/java/PigLatin.java
+++ PigLatin/src/main/java/PigLatin.java
@@ -17,6 +17,9 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        
+            char firstLetter = in.charAt(0);
+            String restOfWord = in.substring(1);
+            return restOfWord + firstLetter + "ay";
     }
 }
