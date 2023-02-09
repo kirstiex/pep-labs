e56157cc5876c8b15b6f15c7a8517a0fe24a7fca
Kirstie Goggans <ikirstiew@gmail.com>
2023-02-09 15:20:56 +0000

    Saved progress on IsThereSum 2023-02-09 15:20:56 +0000

---
+++ IsThereSum/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running IsThereSumTest
+Tests run: 4, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.042 sec
+
+Results :
+
+Tests run: 4, Failures: 0, Errors: 0, Skipped: 0
+
