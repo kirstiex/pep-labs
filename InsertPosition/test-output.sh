44bbc3574ea6aeebca1f25c7ee3c7a04c500f4c6
Kirstie Goggans <ikirstiew@gmail.com>
2023-02-10 00:41:20 +0000

    Saved progress on InsertPosition 2023-02-10 00:41:20 +0000

---
+++ InsertPosition/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running InsertPositionTest
+Tests run: 5, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.04 sec
+
+Results :
+
+Tests run: 5, Failures: 0, Errors: 0, Skipped: 0
+
